c29aa63dc2d73c1fbfd7ac33403f3fd9e3caddaf
Jamie Brown <jamiebrown201@hotmail.com>
2016-02-11 13:32:38 +0000

    refresh

--- web/config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
- provider :fitbit, '', '', scope: 'activity heartrate location nutrition profile settings sleep social weight'
-end
--- web/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'fitbit_auth#index'
-  post "/auth/fitbit" => "fitbit_auth#make_request"
-  get "/auth/fitbit/callback" => "fitbit_auth#get_response"
-end
